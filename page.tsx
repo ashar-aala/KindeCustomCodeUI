999297dd5e2ef41b203d4c0e5728e8177a29e9e1
ashar-aala <ashar@aalasolutions.com>
2025-01-14 11:28:44 +0500

    added pages

---
+++ page.tsx
@@ -0,0 +1,44 @@
+"use server";
+
+import {
+  getKindeCSRF,
+  getKindeRequiredCSS,
+  getKindeRequiredJS,
+  getKindeWidget,
+} from "@kinde/infrastructure";
+import React from "react";
+import { renderToString } from "react-dom/server.browser";
+
+const Layout = async ({ request }) => {
+  return (
+    <html lang={request.locale.lang}>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex" />
+        <meta name="csrf-token" content={getKindeCSRF()} />
+        <title>Document</title>
+        {getKindeRequiredCSS()}
+        {getKindeRequiredJS()}
+      </head>
+      <body>
+        <div id="root" data-roast-root="/admin">
+          <header>
+            <h1>Company Name</h1>
+          </header>
+          <main>
+            <h6>hello world</h6>
+            {getKindeWidget()}
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+};
+
+const Page = async (event) => {
+  const page = await Layout({ ...event });
+  return renderToString(page);
+};
+
+export default Page;
